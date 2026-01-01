a824cc4d54c6ca21933d10f7da46b1cf276832c7
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-01-01 12:23:29 +0530

    Add files via upload

---
+++ int_input.cs
@@ -0,0 +1,9 @@
+using System;
+class Int_input{
+	static void Main(){
+		Console.Write("Enter Value of Number : ");
+		int s = Convert.ToInt16(Console.ReadLine());
+		Console.WriteLine("Value of Number : "+s);
+		Console.ReadLine();
+	}
+}
---
+++ multiple_int_print.cs
@@ -0,0 +1,8 @@
+using System;
+class Int_input{
+	static void Main(){   
+		int s1=5,s2=10,s3=534;
+		Console.WriteLine("s1 = {0}\ns2 = {1}\ns3 = {2}",s1,s2,s3);
+		Console.ReadLine();
+	}
+}
