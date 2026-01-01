9103a9e27d97a626d76de9cebedf00f098c7ccee
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-01-01 11:56:00 +0530

    Add files via upload

---
+++ string_concate.cs
@@ -0,0 +1,10 @@
+using System;
+class strconcat{
+	static void Main(String[] sk){
+		String s = "Sagil";
+		Console.WriteLine(s);
+		s+="kureshi";
+		Console.WriteLine(s);
+		Console.ReadLine();
+	}
+}
