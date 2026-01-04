5bcc848c2148afb834dafb8ca12d2fd24591f3e2
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-01-04 10:27:24 +0530

    Add files via upload

---
+++ practicle_04.cs
@@ -0,0 +1,19 @@
+using System;
+class Datatypes{
+	static void Main(String[] SK){
+		int I = 18;
+		long L = 18012007;
+		char C = 'S';
+		float F = 0.007F;
+		double D = 0.18012007;
+		string S = "S@gil Kureshi";
+		bool B = true;
+		Console.WriteLine("Integer I = "+I);
+		Console.WriteLine("Long L = "+L);
+		Console.WriteLine("Character C = "+C);		
+		Console.WriteLine("Float F = "+F);
+		Console.WriteLine("Double D = "+D);
+		Console.WriteLine("String S = "+S);					
+		Console.WriteLine("Boolean B = "+B);
+	}
+}
---
+++ practicle_05.cs
@@ -0,0 +1,19 @@
+using System;
+class Dynamic{
+	static void Main(String[] SK){
+		dynamic name = "Sagil Kureshi";
+		dynamic age = 18;
+		dynamic first_later = 'S';
+		dynamic marks = 82.67;
+		dynamic enrol_no = 02929;
+		dynamic code = 0.18012007;
+		dynamic ans = true;
+		Console.WriteLine("Name : "+name);
+		Console.WriteLine("Age  : "+age);
+		Console.WriteLine("First later : "+first_later);
+		Console.WriteLine("Marks : "+marks);
+		Console.WriteLine("Enrollment NO. : "+enrol_no);
+		Console.WriteLine("Code : "+code);
+		Console.WriteLine("Answer : "+ans);
+	}
+}
