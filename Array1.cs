6a6f4364af1f2b03cf9b0983e90ac58eb36494c8
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-01-08 12:32:35 +0530

    Add files via upload

---
+++ Array1.cs
@@ -0,0 +1,10 @@
+using System;
+class Array{
+	static void Main(){
+		int[] array = {10,23,32,54,64};
+		for(int s=0;s<array.Length;s++){
+			Console.WriteLine(array[s]);
+		}
+		Console.ReadLine();
+	}
+}
