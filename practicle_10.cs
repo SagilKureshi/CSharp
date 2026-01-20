48ffc1222f76c9e6b81eb879ee9a3977e4125799
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-01-20 08:48:39 +0530

    Update practicle_10.cs

--- practicle_10.cs
+++ practicle_10.cs
@@ -2,10 +2,16 @@ using System;
 class User_Array{
 	static void Main(){
 		Console.Write("Enter size of Array : ");
-		int size = int.ParseI(Console.ReadLine());
+		int size = int.Parse(Console.ReadLine());
+		Console.WriteLine("Enter the Elements(Values) of Array :- \n");
 		int[] arr = new int[size];
-		for(int s=0;s<arr.Length;a++){
-			Console.Wrtie("arr[{s}] : ");
+		for(int s=0;s<arr.Length;s++){
+			Console.Write("arr["+s +"] : ");
+			arr[s] = int.Parse(Console.ReadLine());
 		}
+		Console.WriteLine("Print the Elements(Values) of Array :- \n");		
+		for(int s=0;s<arr.Length;s++){
+			Console.WriteLine("arr["+s +"] = "+arr[s]);
 		}
+	}
 }
