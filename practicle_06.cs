de94f0f1c96d2a0bab4f406734b3cfc46e5ef192
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-01-28 08:48:56 +0530

    Update practicle_06.cs

--- practicle_06.cs
+++ practicle_06.cs
@@ -1,10 +1,14 @@
 using System;
 class strconcat{
-	static void Main(String[] sk){
-		String s = "Sagil";
-		Console.WriteLine(s);
-		s+="kureshi";
-		Console.WriteLine(s);
-		Console.ReadLine();
+	static void Main(){
+		object sk = 18;
+		Console.WriteLine("sk = "+sk);
+		sk = 0.7;
+		Console.WriteLine("sk = "+sk);
+		sk = 'S';
+		Console.WriteLine("sk = "+sk);
+		sk = "Sagil Kureshi";
+		Console.WriteLine("sk = "+sk);
+		Console.WriteLine();
 	}
 }
