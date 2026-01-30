c38ab8b6f3f3185b5055daa89793ffd86e2e7a7e
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-01-30 21:27:32 +0530

    Add files via upload

---
+++ Practicle_17.cs
@@ -0,0 +1,15 @@
+using System;
+class Check_Number{
+	static void Main(){
+		Console.WriteLine("Enter a Number to check that is Positive or Nagative Or Zero :- \n");
+		Console.Write("Enter a Number : ");
+		int SK_Num = int.Parse(Console.ReadLine());
+		
+		if(SK_Num > 0)
+			Console.WriteLine("The Number is Positive");
+		else if(SK_Num < 0)
+			Console.WriteLine("The Number is Nagative");
+		else
+			Console.WriteLine("The Number is Zero");
+	}
+}
