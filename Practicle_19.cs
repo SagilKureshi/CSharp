9bd958ae9ff5841a2b2ff3109da87201433fc95c
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-01-31 09:51:13 +0530

    Update Practicle_19.cs

--- Practicle_19.cs
+++ Practicle_19.cs
@@ -3,20 +3,37 @@ class Menu{
 	static void Main(){
 		char yn;	
 		do{
-			Console.WriteLine("MenuMenu for Watchs :-\n");
-			Console.WriteLine("Press [1] for Rolex Watch");
-			Console.WriteLine("Press [2] for Patek Philippe Watch");
-			Console.WriteLine("Press [3] for Omega Watch");
-			Console.WriteLine("Press [4] for Titan Watch");
-			Console.WriteLine("Press [5] for Casio Watch\n");
+			Console.WriteLine("Menu for Watches :-\n");
+			Console.WriteLine("Press [1] for Rolex Watches");
+			Console.WriteLine("Press [2] for Patek Philippe Watches");
+			Console.WriteLine("Press [3] for Omega Watches");
+			Console.WriteLine("Press [4] for Titan Watches");
+			Console.WriteLine("Press [5] for Casio Watches\n");
 			
 			Console.Write("Your Choice : ");
 			int choice = int.Parse(Console.ReadLine());
 			
 			switch(choice){
 				case 1:
-					Console.WriteLine("Welcome to Rolex Watchs :-");
+					Console.WriteLine("Welcome to Rolex Watches :-");
 					Console.WriteLine("Quantity\t\t\tModel\t\\t\tPrice");
+					break;
+				case 2:
+					Console.WriteLine("Welcome to Patek Philippe Watches :-");
+					Console.WriteLine("Quantity\t\t\tModel\t\\t\tPrice");
+					break;
+				case 3:
+					Console.WriteLine("Welcome to Omega Watches :-");
+					Console.WriteLine("Quantity\t\t\tModel\t\\t\tPrice");
+					break;
+				case 4:
+					Console.WriteLine("Welcome to Titan Watches :-");
+					Console.WriteLine("Quantity\t\t\tModel\t\\t\tPrice");
+					break;	
+				case 5:
+					Console.WriteLine("Welcome to Casio Watches :-");
+					Console.WriteLine("Quantity\t\t\tModel\t\\t\tPrice");
+					break;
 			}
 			Console.Write("Do you want to Continue Press [y/n] : ");
 			yn = char.Parse(Console.ReadLine());
