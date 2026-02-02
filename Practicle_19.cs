7c2682bbdfc99517a2a0a36cc4aa312997dc4222
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-02-02 10:42:44 +0530

    Update Practicle_19.cs

--- Practicle_19.cs
+++ Practicle_19.cs
@@ -1,7 +1,24 @@
 using System;
 class Menu{
+	static int rolex_quant = 0;
+	static int rolex = 32411568;
+	static int patek = 272;
+	static int patek_quant = 0; 
+	static int Omega = 277664000;
+	static int omega_quant = 0;
+	static int Titan = 4000000;
+	static int titan_quant = 0;
+	static int casio = 49995;
+	static int casio_quant = 0;
+		
+	static void Rolex(){
+		Console.WriteLine("Welcome to Rolex Watches :-");
+		Console.WriteLine("Quantity\t\tModel\t\t\t\t\tPrice(INR)");
+		Console.WriteLine("{0}\t\tRolex Daytona Paul Newman 6239\t\t\t32,411,568($17.75 million)",rolex_quant);	
+	}		
 	static void Main(){
-		char yn;	
+	char yn;
+			
 		do{
 			Console.WriteLine("Menu for Watches :-\n");
 			Console.WriteLine("Press [1] for Rolex Watches");
@@ -9,39 +26,37 @@ class Menu{
 			Console.WriteLine("Press [3] for Omega Watches");
 			Console.WriteLine("Press [4] for Titan Watches");
 			Console.WriteLine("Press [5] for Casio Watches\n");
-			
+				
 			Console.Write("Your Choice : ");
 			int choice = int.Parse(Console.ReadLine());
-			
+				
 			switch(choice){
 				case 1:
-					Console.WriteLine("Welcome to Rolex Watches :-");
-					Console.WriteLine("Quantity\t\tModel\t\t\t\t\tPrice(INR)");
-					Console.WriteLine("1\t\tRolex Daytona Paul Newman 6239\t\t\t32,411,568($17.75 million)");
+					Rolex();
 					break;
 				case 2:
-					Console.WriteLine("Welcome to Patek Philippe Watches :-");
-					Console.WriteLine("Quantity\t\tModel\t\t\t\t\tPrice(INR)");
-					Console.WriteLine("1\t\tPatek Philippe Grandmaster Chime Ref. 6300A-010\t\t\t\t\t₹272 crore ($31.1 million)");
+
+						
 					break;
 				case 3:
+						
 					Console.WriteLine("Welcome to Omega Watches :-");
-					Console.WriteLine("Quantity\t\tModel\t\t\t\t\tPrice(INR)");
-					Console.WriteLine("1\t\tOmega vintage Speedmaster Ref. 2915-1\t\t\t\t\t₹27,76,64,000($17.75 million)");
+						Console.WriteLine("Quantity\t\tModel\t\t\t\t\tPrice(INR)");
+						Console.WriteLine("{0}\t\tOmega vintage Speedmaster Ref. 2915-1\t\t\t\t\t₹27,76,64,000($17.75 million)",omega_quant);
 					break;
 				case 4:
-					Console.WriteLine("Welcome to Titan Watches :-");
+						Console.WriteLine("Welcome to Titan Watches :-");
 					Console.WriteLine("Quantity\t\tModel\t\t\t\t\tPrice(INR)");
-					Console.WriteLine("1\t\t Titan Nebula Jalsa\t\t\t\t\t₹40.5 lakh (4.05 million INR)");
-					break;	
+					Console.WriteLine("{0}\t\t Titan Nebula Jalsa\t\t\t\t\t₹4000000 (4.05 million INR)",titan_quant);
+						break;	
 				case 5:
 					Console.WriteLine("Welcome to Casio Watches :-");
 					Console.WriteLine("Quantity\t\tModel\t\t\t\t\tPrice(INR)");
-					Console.WriteLine("1\t\tModel\t\t\t\t\tPrice(INR)");
+					Console.WriteLine("{0}\t\tCASIO G-SHOCK Men Watch\t\t\t\t\t₹49995",casio_quant);
 					break;
 			}
 			Console.Write("Do you want to Continue Press [y/n] : ");
 			yn = char.Parse(Console.ReadLine());
 		}while(yn == 'y');
-	}
+		}
 }
