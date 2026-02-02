712fbf351b691e06665bbc7830e89f092e2c97cc
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-02-02 09:37:36 +0530

    Update Practicle_19.cs

--- Practicle_19.cs
+++ Practicle_19.cs
@@ -16,23 +16,28 @@ class Menu{
 			switch(choice){
 				case 1:
 					Console.WriteLine("Welcome to Rolex Watches :-");
-					Console.WriteLine("Quantity\t\t\tModel\t\\t\tPrice");
+					Console.WriteLine("Quantity\t\tModel\t\t\t\t\tPrice(INR)");
+					Console.WriteLine("1\t\tRolex Daytona Paul Newman 6239\t\t\t32,411,568($17.75 million)");
 					break;
 				case 2:
 					Console.WriteLine("Welcome to Patek Philippe Watches :-");
-					Console.WriteLine("Quantity\t\t\tModel\t\\t\tPrice");
+					Console.WriteLine("Quantity\t\tModel\t\t\t\t\tPrice(INR)");
+					Console.WriteLine("1\t\tPatek Philippe Grandmaster Chime Ref. 6300A-010\t\t\t\t\t₹272 crore ($31.1 million)");
 					break;
 				case 3:
 					Console.WriteLine("Welcome to Omega Watches :-");
-					Console.WriteLine("Quantity\t\t\tModel\t\\t\tPrice");
+					Console.WriteLine("Quantity\t\tModel\t\t\t\t\tPrice(INR)");
+					Console.WriteLine("1\t\tOmega vintage Speedmaster Ref. 2915-1\t\t\t\t\t₹27,76,64,000($17.75 million)");
 					break;
 				case 4:
 					Console.WriteLine("Welcome to Titan Watches :-");
-					Console.WriteLine("Quantity\t\t\tModel\t\\t\tPrice");
+					Console.WriteLine("Quantity\t\tModel\t\t\t\t\tPrice(INR)");
+					Console.WriteLine("1\t\t Titan Nebula Jalsa\t\t\t\t\t₹40.5 lakh (4.05 million INR)");
 					break;	
 				case 5:
 					Console.WriteLine("Welcome to Casio Watches :-");
-					Console.WriteLine("Quantity\t\t\tModel\t\\t\tPrice");
+					Console.WriteLine("Quantity\t\tModel\t\t\t\t\tPrice(INR)");
+					Console.WriteLine("1\t\tModel\t\t\t\t\tPrice(INR)");
 					break;
 			}
 			Console.Write("Do you want to Continue Press [y/n] : ");
