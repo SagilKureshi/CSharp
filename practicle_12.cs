5e0e27678574d758e3a2ae866d7f1314de3be5bc
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-02-07 09:37:49 +0530

    Update practicle_12.cs

--- practicle_12.cs
+++ practicle_12.cs
@@ -1,17 +1,12 @@
 using System;
 class Array_2D{
 	static void Main(){
-		int[,] SK = new int[3,3];
-		Console.WriteLine("Enter the elements(values) of Array :-");
-		
-		//loop for get the Elements(values) of the array from the User
-		for(int s=0;s<SK.GetLength(0);s++){
-			for(int k=0;k<SK.GetLength(1);k++){
-				Console.Write("SK["+s+","+k+"] : ");
-				SK[s,k] = int.Parse(Console.ReadLine());
-			}
-			Console.WriteLine();
-		}
+		int[,] SK = new int[2,2];		
+		SK[0,0] = 1;
+		SK[0,1] = 2;
+		SK[1,0] = 3;
+		SK[1,1] = 4;
+	
 		//loop for print the Elements(values) of the array  
 		for(int s=0;s<SK.GetLength(0);s++){
 			for(int k=0;k<SK.GetLength(1);k++){
