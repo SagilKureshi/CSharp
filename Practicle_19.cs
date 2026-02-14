e6d7fda32f55bdc3ff8659e37d8ef7a4495a4fe9
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-02-14 19:27:21 +0530

    Update Practicle_19.cs

--- Practicle_19.cs
+++ Practicle_19.cs
@@ -30,12 +30,8 @@ class SK_Fast_Food{
 		Console.WriteLine("|-------------------------|");
 		Console.WriteLine("|  {0}\t|  {1}|",name,price+"    ");
 		Console.WriteLine("|-------------------------|");
-		Console.Write("\nDo you Want To Buy [y/n] : ");
-		char ny = char.Parse(Console.ReadLine());
+		Bill(name,price);
 		
-		if(ny == 'y' || ny == 'Y'){
-			Bill(name,price);
-			}
 	}
 	
 	static void Bill(string name,int price){
