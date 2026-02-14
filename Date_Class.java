5bf843c83cb19e3f1e5c2965ed5308d177abb638
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-02-14 10:23:34 +0530

    Add files via upload

---
+++ Date_Class.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Date_Class{
+	public static void main(String SK[]){
+		Date d1 = new Date();
+		System.out.println("Current Date is "+d1);
+		Date d2 = new Date();
+		System.out.println("Date represented is "+d2);
+	}
+}
