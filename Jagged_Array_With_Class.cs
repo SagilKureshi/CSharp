ada9d4948e442f78326c63cda35819a48ae22fed
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-02-21 09:25:41 +0530

    Update Jagged_Array_With_Class.cs

--- Jagged_Array_With_Class.cs
+++ Jagged_Array_With_Class.cs
@@ -1,21 +1,38 @@
 using System;
-class Parent{
+public class Parent{
     public int[][] SK  = new int[5][];
-}
-class Child : Parent{
-    public void getsize()
-    {
+    public Parent(){
        SK[0] = new int[5];
        SK[1] = new int[1];
        SK[2] = new int[3];
        SK[3] = new int[1];
        SK[4] = new int[5];
     }
-
+    
 }
-class SK{
+public class Child : Parent{   
+    public void Show_Array()
+    {
+         for(int s = 0; s < SK.Length; s++)
+        {
+            for(int k = 0; k < SK[s].Length; k++)
+            {
+                Console.Write(SK[s][k] + " ");
+            }
+            Console.WriteLine();
+        }   
+    }
+}
+public class SK{
     public static void Main(){
         Child obj = new Child();
-        obj.getsize();
+        for(int s = 0; s < obj.SK.Length; s++)
+        {
+            for(int k = 0; k < obj.SK[s].Length; k++)
+            {
+                obj.SK[s][k] = 1;
+            }
+        }    
+        obj.Show_Array();
     }
 }
