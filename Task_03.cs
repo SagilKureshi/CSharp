7450a90961f71a0f0ad279a189cc4c0a7b2c3a86
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-02-23 17:15:33 +0530

    Add files via upload

---
+++ Task_03.cs
@@ -0,0 +1,24 @@
+    using System;
+    class Parent{
+        protected int square(int s){
+            return (s*s);
+        }
+    } 
+    class Child : Parent{
+        public int child(int s){
+            return square(s);
+        }
+    }
+    class Sub_Child : Child{
+        public int sub_child(){
+            Console.Write("Enter a Value to find it's Square : ");
+            return child(int.Parse(Console.ReadLine()));
+        }
+    }
+    class SK{
+        public static void Main(){
+            Sub_Child sb = new Sub_Child();
+            Console.WriteLine("Square = " + (sb.sub_child()));
+            Console.ReadLine();
+        }
+    }
