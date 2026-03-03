685a00a8780be13febbc2f0cdb35dc4faef441e0
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-03-03 17:20:10 +0530

    Update Practical_33.cs

--- Practical_33.cs
+++ Practical_33.cs
@@ -1,7 +1,7 @@
 using System;
 class SK{
     string name;
-    public string property_name_1{
+    public string Property_1{// Read - Write Both Property
         get{
             return name;
         }
@@ -9,18 +9,38 @@ class SK{
             name = value;
         }
     }
-public string property_name_2
+    public string Property_2 // Read Only Property
     {
-        
+        get{
+            return name;
+        }
+        public void assign(){
+            Console.WriteLine("Enter Your Name : ");
+            name = Console.ReadLine();
+        }
+    }
+    public string Property_3 // Write Only Property
+    {
+        set{
+            name = value;
+        }
+        public void show(){
+            Console.WriteLine("Your Name Is : " + name);
+        }
     }
 }
 class Properties{
     public static void Main(){
         SK obj = new SK();
         Console.Write("Enter Your name :- ");
-        obj.property_name = Console.ReadLine();
+        obj.Property_1 = Console.ReadLine();
         Console.Write("Your name is :- ");
-        Console.WriteLine(obj.property_name);
+        Console.WriteLine(obj.Property_1);
+        obj2.assign();
+        obj2.Property_2;
+        Console.WriteLine("Enter Your Name : ");
+        obj.Property_3 = Console.ReadLine();
+        obj.show();
         Console.ReadLine();
     }
 }
