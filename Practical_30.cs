a25ccbba3c2d965f57b6715e488650e21ca7abf2
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-03-04 18:14:04 +0530

    Update Practical_30.cs

--- Practical_30.cs
+++ Practical_30.cs
@@ -1,24 +1,40 @@
 //Hierarchical Interface Inheritance
 using System;
-interface Interface{ 
-    void Parent_fun();
+interface Parent{
+    void parent_interface_fun();
 }
-class S : Interface{
-    public void Parent_fun(){
-        Console.WriteLine("Parent Interface Method Using Class A ");
+interface Child_1 : Parent{
+    void child1_interface_fun();
+}
+interface Child_2 : Parent{
+    void child2_interface_fun();
+}
+class class_1 : Child_1{
+    public void parent_interface_fun(){
+        Console.WriteLine("Parent Interface Function");
+    }
+    public void child1_interface_fun(){
+        Console.WriteLine("Child 1 Interface Function");
     }
 }
-class K : Interface{
-    public void Parent_fun(){
-        Console.WriteLine("Parent Interface Method Using Class B ");
+class class_2 : Child_2{
+    public void parent_interface_fun(){
+        Console.WriteLine("Parent Interface Function");
+    }
+    public void child2_interface_fun(){
+        Console.WriteLine("Child 2 Interface Function");
     }
 }
-class Hierarchical_Interface{
+class Hierarchical_Interface_Inheritance{
     public static void Main(){
-        S S_obj = new S();
-        S_obj.Parent_fun();
-        K K_obj = new K();
-        K_obj.Parent_fun();
+        class_1 obj1 = new class_1();
+        class_2 obj2 = new class_2();
+        Console.WriteLine("Class 1 :- ");
+        obj1.parent_interface_fun();
+        obj1.child1_interface_fun();
+        Console.WriteLine("\nClass 2 :- ");
+        obj2.parent_interface_fun();
+        obj2.child2_interface_fun();
         Console.ReadLine();
     }
 }
