4b9966c741cc9d99706814a023133afa6de02eaa
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-03-07 09:49:17 +0530

    Enhance indexers with bounds checking and 2D support
    Added bounds checking for 1D array indexer and implemented 2D array element assignment and printing.

--- Indexer_Overloading.cs
+++ Indexer_Overloading.cs
@@ -9,10 +9,13 @@ class SK{
     }
     public int this[int s]{
         get{
-            return Arr_1d[s];
+            if(s>=0 && s<Arr_1d_size)
+                return Arr_1d[s];
+            return 0;
         }
         set{
-            Arr_1d[s] = value;
+            if(s>=0 && s<Arr_1d_size)
+                Arr_1d[s] = value;
         }
     }
     public int this[int s,int k]{
@@ -32,10 +35,26 @@ class Indexer_Overloading{
         obj[2] = 30;
         obj[3] = 40;
         obj[4] = 50;
-        Console.WriteLine("Printing of Arr_1d's Elements :- ");
+        Console.WriteLine("Printing of Arr_1d's Elements :- \n");
 
         for(int s=0;s<obj.Arr_1d_size;s++){
             Console.WriteLine("Arr_1d[{0}] = {1} " ,s,obj[s]);
+        }
+        obj[0,0] = 1;
+        obj[0,1] = 2;   
+        obj[0,2] = 3;
+        obj[1,0] = 4;
+        obj[1,1] = 5;
+        obj[1,2] = 6;
+        obj[2,0] = 7;
+        obj[2,1] = 8;
+        obj[2,2] = 9;
+        Console.WriteLine("\nPrinting of Arr_2d's Elements :- \n");   
+        for(int s=0;s<3;s++){
+            for(int k=0;k<3;k++){
+                Console.Write("Arr_2d[{0},{1}] = {2} ",s,k,obj[s,k]);
+            }
+            Console.WriteLine();
         }
         Console.ReadLine();
     }
